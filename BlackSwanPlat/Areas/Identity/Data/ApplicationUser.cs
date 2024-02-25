782f17b7f9eb0ff0e34cc178ded87318abac6e6a
payam1266 <payamghaznavi24@gmail.com>
2024-02-25 13:44:16 +0330

    Add project files.

---
+++ BlackSwanPlat/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlackSwanPlat.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string city { get; set; }
+    public int age { get; set; }
+}
+
---
+++ BlackSwanPlat/Program.cs
@@ -0,0 +1,19 @@
+namespace BlackSwanPlat
+{
+
+    public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
+}
+    
